ebb0682a9015be19f0e9192154878bc2e9695246
Meshari <mesharialharbi@Mesharis-MacBook-Pro.local>
2019-02-17 18:18:36 -0700

    Using openweathermap API Working

--- app.js
+++ app.js
@@ -1,79 +1,37 @@
-//Get the location of the device
-function place() {
-  let long;
-  let lat;
-  let search;
+let searchInput = document.getElementById("search_input");
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-
-      //   const api = `${proxy}https://api.darksky.net/forecast/46be7b311c7390f8f3da28513525abed/${lat},${long}`;
-      const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=a339e025a95d6f5ed4cb3251bcfd566d&units=imperial`;
-      // Fetching the data from the api
-      fetch(api)
-        .then(data => {
-          // Converting the data to JSON
-          return data.json();
-        })
-        .then(data => {
-          console.log(data);
-          // Call a fun
-          results(data);
-        });
-    });
+// Getting the value from input
+document.querySelector(".search__btn").addEventListener("click", function() {
+  let searchValue = searchInput.value;
+  // Check value
+  if (searchValue <= 0) {
+    // console.log("No input");
   } else {
-    alert("Geolocation is not supported by this browser");
+    getWeather(searchValue);
+    searchInput.value = "";
   }
+});
 
-  function results(data) {
-    // Change BG
-    switch (data.weather[0].main) {
-      case "Thunderstorm":
-        document.getElementById("myDiv").style.backgroundImage =
-          "url('Thunderstorm.jpg')";
-        break;
-
-      case "Drizzle":
-      case "Rain":
-        document.getElementById("myDiv").style.backgroundImage =
-          "url('Rain.jpeg')";
-        break;
-
-      case "Snow":
-        document.getElementById("myDiv").style.backgroundImage =
-          "url('Snow.jpg')";
-        break;
-
-      case "Clouds":
-        document.getElementById("myDiv").style.backgroundImage =
-          "url('Cloudy.jpg')";
-        break;
-
-      case "Clear":
-        document.getElementById("myDiv").style.backgroundImage =
-          "url('Clear.jpeg')";
-        break;
-
-      default:
-        break;
-    }
-    console.log(`Weather is ${data.weather[0].main}`);
-
-    let city = document.getElementById("city");
-    let weatherDescription = document.getElementsByClassName(
-      ".weatherDescription"
-    );
-    let tempDegree = document.getElementById("degree");
-    let sunrise = document.getElementsByClassName(".sunrise");
-    let sunset = document.getElementsByClassName(".sunset");
-    let sunriseTime = document.getElementsByClassName(".sunriseTime");
-    let sunsetTime = document.getElementsByClassName(".sunsetTime");
-
-    city.innerHTML = data.name;
-    tempDegree.innerHTML = Math.floor(data.main.temp) + "&#176"; //"&#176" temp symbol
-  }
+// Get weather data from api
+function getWeather(searchValue) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=a339e025a95d6f5ed4cb3251bcfd566d&units=imperial`
+  )
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data);
+      // Change city name from defult to input
+      document.getElementById("city").innerHTML = data.name;
+      document.getElementById("weatherDescription").innerHTML =
+        data.weather[0].description;
+      document.getElementById("degree").innerHTML = Math.floor(data.main.temp);
+      document.querySelector(".weather__temp--symbol").style.visibility =
+        "visible";
+      document.querySelector(".temp__low").innerHTML = Math.floor(
+        data.main.temp_min
+      );
+      document.querySelector(".temp__high").innerHTML = Math.floor(
+        data.main.temp_max
+      );
+    });
 }
-
-place();
